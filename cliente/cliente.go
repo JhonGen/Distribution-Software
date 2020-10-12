9ee397bcc089533a123aa02f884cf9cb55883843
ionate <ignacio.onate@sansano.usm.cl>
2020-10-12 11:38:19 -0300

    weando puertos

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("10.10.28.47", grpc.WithInsecure())
+	conn, err := grpc.Dial("10.10.28.47:4040", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
