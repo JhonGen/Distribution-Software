08fabbe2ba2f15ff6cde2b86e3e69ad580070901
ionate <ignacio.onate@sansano.usm.cl>
2020-10-12 11:46:30 -0300

    puse la ip

--- logistica/logistica.go
+++ logistica/logistica.go
@@ -15,7 +15,7 @@ type LogisticaServer struct {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:4040")
+	listener, err := net.Listen("tcp", "10.10.28.47:4040")
 	if err != nil {
 		panic(err)
 	}
