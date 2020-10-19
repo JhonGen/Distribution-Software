f9860bfcb1f642f3dce9e286364790c5adcbf7b3
ionate <ignacio.onate@sansano.usm.cl>
2020-10-19 02:41:24 -0300

    financiero casi listo pero no a prueba de fallas

--- financiero/financiero.go
+++ financiero/financiero.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/streadway/amqp"
 )
@@ -19,13 +23,82 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func HacerCalculos(mensaje []byte) {
+func HacerCalculos(mensaje []byte, balance []float64) []float64 {
 	mensajeStruct := MensajeFinanzas{}
 	json.Unmarshal(mensaje, &mensajeStruct)
+	valor, _ := strconv.ParseFloat(mensajeStruct.Order[2], 64)
+	intentos, _ := strconv.ParseFloat(mensajeStruct.Order[6], 64)
+	if mensajeStruct.Order[3] == "pyme" {
+		if mensajeStruct.Status == "No entregado" && mensajeStruct.Order[5] == "true" {
+			balance = append(balance, valor*0.3-10*intentos)
+			ganancia := fmt.Sprintf("%f", valor*0.3-10*intentos)
+			f, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			_, err = f.WriteString("Pedido: " + mensajeStruct.Order[0] + "(" + mensajeStruct.Order[1] + ") " + mensajeStruct.Status + "Balance:" + ganancia)
+			if err != nil {
+				log.Fatal("whoops")
+			}
+			err = f.Close()
 
+		} else if mensajeStruct.Status == "No entregado" && mensajeStruct.Order[5] == "false" {
+			balance = append(balance, -10*intentos)
+			ganancia := fmt.Sprintf("%f", -10*intentos)
+			f, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			_, err = f.WriteString("Pedido: " + mensajeStruct.Order[0] + "(" + mensajeStruct.Order[1] + ") " + mensajeStruct.Status + "Balance:" + ganancia)
+			if err != nil {
+				log.Fatal("whoops")
+			}
+			err = f.Close()
+		} else if mensajeStruct.Status == "Entregado" && mensajeStruct.Order[5] == "true" {
+			balance = append(balance, valor*1.3-10*intentos)
+			ganancia := fmt.Sprintf("%f", valor*1.3-10*intentos)
+			f, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			_, err = f.WriteString("Pedido: " + mensajeStruct.Order[0] + "(" + mensajeStruct.Order[1] + ") " + mensajeStruct.Status + "Balance:" + ganancia)
+			if err != nil {
+				log.Fatal("whoops")
+			}
+			err = f.Close()
+
+		} else if mensajeStruct.Status == "Entregado" && mensajeStruct.Order[5] == "false" {
+			balance = append(balance, valor*1.3-10*intentos)
+			ganancia := fmt.Sprintf("%f", valor*1.3-10*intentos)
+			f, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			_, err = f.WriteString("Pedido: " + mensajeStruct.Order[0] + "(" + mensajeStruct.Order[1] + ") " + mensajeStruct.Status + "Balance:" + ganancia)
+			if err != nil {
+				log.Fatal("whoops")
+			}
+			err = f.Close()
+
+		}
+	} else if mensajeStruct.Order[3] == "retail" {
+		balance = append(balance, valor-10*intentos)
+		ganancia := fmt.Sprintf("%f", valor-10*intentos)
+		f, err := os.OpenFile("registro.txt", os.O_APPEND|os.O_WRONLY, 0644)
+		_, err = f.WriteString("Pedido: " + mensajeStruct.Order[0] + "(" + mensajeStruct.Order[1] + ") " + mensajeStruct.Status + "Ganancia:" + ganancia)
+		if err != nil {
+			log.Fatal("whoops")
+		}
+		err = f.Close()
+	}
+	return balance
 }
-
+func ObtenerGanancia(linea string) float64 {
+	lineaSeparada := strings.Split(linea, ":")
+	flotante, _ := strconv.ParseFloat(lineaSeparada[2], 64)
+	return flotante
+}
 func main() {
+	var balanceGeneral []float64
+	file, err2 := os.Open("registro.txt")
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ganancia := ObtenerGanancia(scanner.Text())
+		balanceGeneral = append(balanceGeneral, ganancia)
+	}
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -45,8 +118,7 @@ func main() {
 	forever := make(chan bool)
 	go func() {
 		for d := range mensajes {
-			fmt.Printf("mensaje recibido: %s\n", d.Body)
-			HacerCalculos(d.Body)
+			balanceGeneral = HacerCalculos(d.Body, balanceGeneral)
 		}
 	}()
 
